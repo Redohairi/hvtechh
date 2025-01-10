7d2c21228bea9b768d6f3b0764062ed90ef0a574
Redohairi <daviborges07@gmail.com>
2025-01-09 21:38:24 -0300

    angular-todo

---
+++ todolist/angular-todo/src/app/pages/todo/todo.component.ts
@@ -0,0 +1,90 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormGroup, FormControl, Validators,ReactiveFormsModule } from '@angular/forms';
+import { Tarefa } from  '/home/davi/aulashv/todolist/angular-todo/src/app/models/tarefas.models';
+import { TarefaService } from '/home/davi/aulashv/todolist/angular-todo/src/app/services/tarefa.service';
+import { MatSnackBarModule, MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-todo',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatSnackBarModule
+  ],
+  templateUrl: './todo.component.html',
+  styleUrl: './todo.component.css'
+})
+export class TodoComponent implements OnInit {
+
+   //formulario reativo para criar tarefas
+   formNovaTarefa!: FormGroup;
+
+   //array de tarefas que vao da tela 
+    tarefas: Tarefa[] = [];
+
+    //status  do filtro
+
+    filtro: 'todas' | 'pendentes' | 'concluidas' = 'todas';
+
+    constructor(
+               private snackBar: MatSnackBar,
+               private tarefaService: TarefaService 
+              ){ }
+
+    ngOnInit(): void {
+
+      //init form
+      this.formNovaTarefa = new FormGroup({
+        titulo: new FormControl('',[ Validators.required, Validators.minLength(3)])
+      });
+
+      //carrega tarefas iniciais
+      this.carregarTarefas();
+    }
+    
+     //Carrega tarefas atuais do serviço
+    carregarTarefas():void{
+      this.tarefas = this.tarefaService.getTarefas(this.filtro);
+    }
+
+    //cria uma nova tarefa se for valido
+
+    adicionar():void{
+      if (this.formNovaTarefa.valid){
+        const titulo = this.formNovaTarefa.get('titulo')?.value;
+        this.tarefaService.criarTarefa(titulo)
+        this.formNovaTarefa.reset();
+        this.carregarTarefas();
+        this.snackBar.open('Deu certo!!!!!!!!><><><<><><><>>','fechar', {
+          duration: 2000
+        });
+      }else{
+        this.snackBar.open('Deu errado!!!!!!!!><><><<><><><>>','fechar', {
+          duration: 2000
+        });
+      }
+    }
+    
+    /**Altera status de uma tarefa
+     * @param tarefa tarefa que vai ser alterada
+     */
+    alterarStatus(tarefa: Tarefa): void{
+      this.tarefaService.alterarStatusTarefa(tarefa.id, !tarefa.concluida);
+      this.carregarTarefas();
+
+      const msg = tarefa.concluida ? 'tarefa reaberta': 'tarefa concluida';
+      this.snackBar.open(msg,'fechar',{
+        duration: 2000
+      });
+    }
+
+    /**Define o filtro de visualização de tarefas
+    * @param f filtro do tipo 'todas' | 'pendentes' | 'concluidas
+    */
+   definirFiltro(f: 'todas' | 'pendentes' | 'concluidas'): void{
+     this.filtro = f;
+     this.carregarTarefas();
+   }
+}
---
+++ todolist/angular-todo/src/app/services/tarefa.service.ts
@@ -0,0 +1,94 @@
+import { Injectable } from "@angular/core";
+import { Tarefa } from '/home/davi/aulashv/todolist/angular-todo/src/app/models/tarefas.models';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class TarefaService {
+
+    /**
+     * Nome da chave do Local Storage que é a camada de persistencia.
+     * Readonly para nao mudar o valor em tempo de execucao.
+     * Private para não ser acessado de fora da classe.
+     */
+    private readonly LOCAL_STORAGE_KEY = 'listaTarefas';
+
+    constructor() { }
+
+    public getTarefas(
+        status: 'todas' | 'pendentes'  | 'concluidas' = 'todas'
+    ): Tarefa[] {
+            const tarefas = this.lerTarefasDoLocalStorage();
+
+            switch (status) {
+                case 'pendentes':
+                    return tarefas.filter(t => !t.concluida);
+                case 'concluidas':
+                    return tarefas.filter(t => t.concluida);
+                default:
+                    return tarefas;
+    }
+}
+  /**
+   * Cria uma nova tarefa e a salva no localStorage.
+   * Retorna a tarefa recém-criada.
+   * @param titulo Texto que descreve a tarefa.
+   */
+
+    public criarTarefa(titulo: string): Tarefa {
+        const tarefas = this.lerTarefasDoLocalStorage();
+        const novaTarefa: Tarefa = {
+            id: Date.now(),
+            titulo,
+            concluida: false
+        };
+    
+        tarefas.push(novaTarefa);
+        this.escreverTarefasNoLocalStorage(tarefas);
+    
+        return novaTarefa;
+
+    }
+
+    /**
+     * Altera o status de uma tarefa para concluída
+     * @param id ID da tarefa a ser concluída
+     * @param concluida Valor de status final: true (concluido) false (pendente)
+     */
+
+    public alterarStatusTarefa(id: number, concluida: boolean): void {
+        //procura todas as tarefas
+        const tarefas = this.lerTarefasDoLocalStorage();
+
+        //pega o indice da tarefa que vai ser alterada
+        const index = tarefas.findIndex(t => t.id === id);
+
+        //se existir a tarefa com o id passado, altera o status
+        if(index !== -1){
+            tarefas[index].concluida = concluida;
+            this.escreverTarefasNoLocalStorage(tarefas);
+        }
+
+    }
+    /**
+     * Le as tarefas do localstorage e retorna uma array de tarefas
+     * @returns Array de tarefas
+     */
+
+    private lerTarefasDoLocalStorage(): Tarefa[] {
+        const tarefas = localStorage.getItem(this.LOCAL_STORAGE_KEY);
+        return tarefas ? JSON.parse(tarefas) : [];
+    }
+    
+
+    
+     //Converte um array de tarefas em json e salva
+
+    private escreverTarefasNoLocalStorage(tarefas: Tarefa[]): void{
+        localStorage.setItem(
+            this.LOCAL_STORAGE_KEY,
+            JSON.stringify(tarefas)
+        )
+    };
+
+}
