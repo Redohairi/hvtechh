1bd35e85ee11d8daac469898d0c7d985196893e4
Redohairi <daviborges07@gmail.com>
2025-02-01 14:31:05 -0300

    botao para redirecionar ao crud

--- projeto5/webapp-frontend/src/app/home.component.ts
+++ projeto5/webapp-frontend/src/app/home.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-
-  template: '<h1>Welcome to the Home Page</h1>'
+  standalone: true,
+  imports: [RouterModule],
+  template: `
+    <h1>Welcome to the Home Page</h1>
+    <button [routerLink]="['/products']">CRUD</button>
+  `
 })
 export class HomeComponent {
 }
